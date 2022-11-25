572297c6b32b7b3863463d2ce3c04cf742266e85
전지훈 <junji636@gmail.com>
2022-11-25 23:25:03 +0900

    first commit

---
+++ SwiftUI_headerAnimation/SwiftUI_headerAnimation/SwiftUI_headerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_headerAnimationApp.swift
+//  SwiftUI_headerAnimation
+//
+//  Created by 전지훈 on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_headerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
